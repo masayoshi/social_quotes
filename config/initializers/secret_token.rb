70c1ed9241402638bb03eb12733d8eda2f1a3581
masay0shi <masayoshi.cdm@gmail.com>
2011-10-29 21:07:56 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialQuotes::Application.config.secret_token = 'afec4275a78aa24224c5a6d5d86a4fb5e0c2bb5a07d89108b943b3799ccb11c279096f8f62fe4e4dc16936fa73cc613fcbd960d097a3c654e9424dbddbc7403a'
