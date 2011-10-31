b12cc85dbf5170a069587466af2f5787850d2f89
masay0shi <masayoshi.cdm@gmail.com>
2011-10-30 22:53:46 -0700

    Configured SendGrid2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,12 +63,13 @@ SocialQuotes::Application.configure do
 
   # Configuration for SendGrid
   ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "25",
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['SENDGRID_DOMAIN']
+    :domain         => 'heroku.com'
   }
+  ActionMailer::Base.delivery_method = :smtp
 
 end
